9596dcebef893aa9ea49ec2fc3912b712e57b26d
Песецкий Никита Андреевич <95304979+FFrein@users.noreply.github.com>
2024-03-07 22:20:10 +0300

    update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,14 @@ import Button from "../buttons/button";
 import { useTg } from "../hooks/useTg";
 
 const Header = (props)=>{
+    
+    const {tg, user, onColse} = useTg();
 
     return (
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {}
+                {user?.username}
             </span>
         </div>
     )
