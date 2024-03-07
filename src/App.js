c8b364b703dbacb3fb3e91c59402b02c771b9017
Песецкий Никита Андреевич <95304979+FFrein@users.noreply.github.com>
2024-03-07 23:25:44 +0300

    update

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
 */
   return (
     <div className="App">
+      LOVE
       <Header></Header>
       <button onClick={onToggleButton}>toggle</button>
     </div>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Button from "../buttons/button";
 import { useTg } from "../hooks/useTg";
+import './header.css'
+import love from '../img/Снимо2.PNG'
 
 const Header = (props)=>{
     
@@ -8,6 +10,8 @@ const Header = (props)=>{
 
     return (
         <div className={'header'}>
+            <img src={love} alt="" />
+            <br/>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
                 {user?.username}
